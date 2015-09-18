f7c5ff4019c498e3628966da6cab12f0c186e9cf
Alexander <lonsas@gmail.com>
2015-09-18 11:39:05 +0200

    Proper test case class

--- testcases.py
+++ testcases.py
@@ -1,20 +1,39 @@
+import unittest
 from math import *
 import bSpline
-import numpy 
-#xtmp = numpy.linspace(0,2*pi,5)
-xtmp = numpy.array([37,73,42,7,3])
-x = numpy.append(xtmp[0],xtmp[0])
-x = numpy.append(x,xtmp)
-x = numpy.append(x,x[-1])
-x = numpy.append(x,x[-1])
-y = numpy.cos(x)
-d = numpy.array([x,y])
+import numpy
+import warnings
+warnings.filterwarnings('error')
 
-gridtmp = numpy.linspace(0,1,5)
-grid = numpy.append(gridtmp[0],gridtmp[0])
-grid = numpy.append(grid,gridtmp)
-grid = numpy.append(grid,grid[-1])
-grid = numpy.append(grid,grid[-1])
-print(grid)
-spline = bSpline.BSpline(grid, d)
-spline.plot()
+class testBSpline(unittest.TestCase):
+	def testAlphaDivideByZero(self):
+		a = bSpline.BSpline(numpy.array([0,0,0,1,1,1]),numpy.array([[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]))
+		try:
+			result = a.alpha(3,3,1)
+		except:
+			self.fail("If nodes coincide 0/0 = 0, but we got division by zero error")
+		expected = 0
+		self.assertEqual(result, expected)
+	def LooksGood(self):
+		#xtmp = numpy.linspace(0,2*pi,5)
+		xtmp = numpy.array([37,73,42,7,3])
+		x = numpy.append(xtmp[0],xtmp[0])
+		x = numpy.append(x,xtmp)
+		x = numpy.append(x,x[-1])
+		x = numpy.append(x,x[-1])
+		y = numpy.cos(x)
+		d = numpy.array([x,y])
+
+		gridtmp = numpy.linspace(0,1,5)
+		grid = numpy.append(gridtmp[0],gridtmp[0])
+		grid = numpy.append(grid,gridtmp)
+		grid = numpy.append(grid,grid[-1])
+		grid = numpy.append(grid,grid[-1])
+		print(grid)
+		spline = bSpline.BSpline(grid, d)
+		spline.plot()
+		self.assertTrue(True)
+
+if __name__== '__main__':
+	unittest.main()
+
