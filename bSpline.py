2efa8423320fec5002c8065910e50d7808836709
lonsas <lonsas@gmail.com>
2015-09-21 18:14:01 +0200

    Tried to fix interpolation and basisfunction. Cleaning up still needed

--- bSpline.py
+++ bSpline.py
@@ -17,11 +17,12 @@ class BSpline(object):
     def basisFunction(cls, u, k, j):
         L = numpy.size(u) - 1 #If we ever run into problem, the first thing to do is uncomment this line, because it really can mess up things.
         if k == 0:
-            if (u[j-1] == u[j]): #whatever counts as equal to
-                return lambda x: 0        
-            return lambda x: ((x>=u[j-1])-(x>=u[j]))
+            return lambda x: (x>=u[j-1] and x<u[j])
         else: 
-            return lambda x: (-1)*cls.alpha(u, u[(j-1)%L], u[(j+k-1)%L])(x)*BSpline.basisFunction(u, (k-1), j%L)(x) - cls.alpha(u, u[j%L], u[(j+k)%L])(x)*BSpline.basisFunction(u, k-1, (j+1)%L)(x)
+            #return lambda x: cls.alpha(u, u[(j+k-1)%L], u[(j-1)%L])(x)*BSpline.basisFunction(u, (k-1), j%L)(x) + cls.alpha(u, u[j%L], u[(j+k)%L])(x)*BSpline.basisFunction(u, k-1, (j+1)%L)(x)
+            return lambda x: cls.alpha(u, u[(j+k-1)], u[(j-1)])(x)*BSpline.basisFunction(u, (k-1), j)(x) + cls.alpha(u, u[j], u[(j+k)])(x)*BSpline.basisFunction(u, k-1, (j+1))(x)
+            
+            #return lambda x: (x-u[j-1])/(u[j+k-1] - u[j-1])*cls.basisFunction(u,k-1,j)(x) + (u[j+k]-x)/(u[j+k]-u[j])*cls.basisFunction(u,k-1,j+1)(x)
 
     def findS(self, i, t):
         #intressanta d: dvalues[i-2:i+1]
@@ -30,7 +31,7 @@ class BSpline(object):
         second3 = self.theD(self.d[:,i], self.d[:,i+1], i, i+3, t)
         last1 = self.theD(second1, second2, i-1, i+1, t)
         last2 = self.theD(second2, second3, i, i+2, t)
-        return self.theD(last1, last2, i, i +1, t)	
+        return self.theD(last1, last2, i, i +1, t)
 
     def theD(self, d1, d2, i_l, i_r, t):
         return (self.alpha(self.grid, i_l, i_r)(t)*d1 + (1-self.alpha(self.grid, i_l, i_r)(t))*d2)
@@ -55,29 +56,29 @@ class BSpline(object):
         xy = numpy.zeros((2, points-1))
         for j, t in enumerate(numpy.linspace(self.grid[0], self.grid[-1],points)[0:-1]):
             i = self.findHotInterval(t)
-            print(i)
             xy[:,j] = self.findS(i, t)
-            print(j)
-        print(points-1)
-        print(xy)
         plt.plot(xy[0,:],xy[1,:])
         plt.plot(self.d[0,:],self.d[1,:])
         plt.show()
     
-    @classmethod	
+    @classmethod
     def interpolation(cls, grid, xy):
-	# Skapa xi
+        # Skapa xi
+        tmpgrid = grid
+        grid = grid[1:-1]
         xi  = numpy.array((grid[:-2]+grid[1:-1]+grid[2:])/3)
+        grid = tmpgrid
         L = numpy.size(xi)
         # utvärdera splinevärdena på alla xchi (men det kanske inte behövs med alla xchi)?
-        print(xi)
-        print(xi[1])
-        print(BSpline.basisFunction(grid, 0, 2)(xi[1]))
-        print(BSpline.basisFunction(grid, 3, 2)(xi[0])) 
-        N = numpy.array([[BSpline.basisFunction(grid, 3, j)(xi[i]) for i in range(0,L)] for j in range(0,L)]).T
-	# skapa ekvationssystemet 	
+        #print(xi)
+        #print(xi[1])
+        #print(BSpline.basisFunction(grid, 0, 2)(xi[1]))
+        #print(BSpline.basisFunction(grid, 3, 2)(xi[0])) 
+        N = numpy.array([[BSpline.basisFunction(grid, 3, j+1)(xi[i]) for i in range(0,L)] for j in range(0,L)]).T
+        # skapa ekvationssystemet
+        print(numpy.size(N))
         print(N)
         dx = scipy.linalg.solve(N, xy[0,:])  #detta bör bytas mot solve_banded när vi fattar hur saker funkar. 	
         dy = scipy.linalg.solve(N, xy[1,:])  #detta också.
-        d = array([dx,dy])
-        return d
+        d = numpy.array([dx,dy])
+        return [xi,d]
--- testcases.py
+++ testcases.py
@@ -32,18 +32,45 @@ class testBSpline(unittest.TestCase):
         grid = numpy.append(grid,gridtmp)
         grid = numpy.append(grid,grid[-1])
         grid = numpy.append(grid,grid[-1])
-        print(grid)
         spline = bSpline.BSpline(grid, d)
         spline.plot()
         self.assertTrue(True)
     def testInterpolation(self):
         xy = numpy.array([[1, 0, 4, -1],[1, -3, 2, -1]])
-        u = numpy.array([0,0,0, 1, 1, 1])
-        d = bSpline.BSpline.interpolation(u, xy)
-        spline = bSpline.BSpline(u, d)
+        #u = numpy.array([0,0,1, 2, 3, 31])
+        u = numpy.array([0,1,2,3,4,5,6,7])
+        xi,dtmp = bSpline.BSpline.interpolation(u, xy)
+        d0tmp = dtmp[0]
+        d1tmp = dtmp[1]
+        d = numpy.append(d0tmp[0],d0tmp[0])
+        d = numpy.append(d,d0tmp)
+        d = numpy.append(d,d[-1])
+        d0 = numpy.append(d,d[-1])
+        d = numpy.append(d0tmp[1],d0tmp[1])
+        d = numpy.append(d,d1tmp)
+        d = numpy.append(d,d[-1])
+        d1 = numpy.append(d,d[-1])
+        gridtmp = xi
+        grid = numpy.append(gridtmp[0],gridtmp[0])
+        grid = numpy.append(grid,gridtmp)
+        grid = numpy.append(grid,grid[-1])
+        grid = numpy.append(grid,grid[-1])    
+        spline = bSpline.BSpline(grid, numpy.array([d0,d1]))
         spline()
-        plt.plot(xy[0,:],xy[1,:])
+        #plt.plot(xy[0,:],xy[1,:])
+        #plt.show()
+    def testBasisFunction(self):
+        points = 10000
+        y = numpy.empty(points)
+        xa = numpy.linspace(0,7,points)
+        for i,x in enumerate(xa):
+            y[i] = (bSpline.BSpline.basisFunction(numpy.array([0,1,2,3,4,5,6,7,7]),3,3)(x))
+        plt.plot(xa,y)
         plt.show()
+        self.assertAlmostEqual(y[numpy.isclose(xa,2,1e-3).argmax()],0, msg="basis function not zero at beginning of its relevant interval")
+        self.assertAlmostEqual(y[numpy.isclose(xa,4,1e-4).argmax()],2/3.0,places=4, msg="basis function not correct in the middle of its relevant interval")
+        self.assertAlmostEqual(y[numpy.isclose(xa,6,1e-3).argmax()],0, msg="basis function not zero at end of its relevant interval")
+
 
 
 if __name__== '__main__':
